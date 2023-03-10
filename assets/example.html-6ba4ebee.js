5958d5b1716014140d02d10f1e5e7627f201e180
ru12321 <48949833+ru12321@users.noreply.github.com>
2023-03-10 18:27:15 +0800

    Delete assets directory

--- assets/example.html-6ba4ebee.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,W as t,X as c}from"./framework-52f8fb67.js";const _={};function r(a,n){return t(),c("div")}const l=e(_,[["render",r],["__file","example.html.vue"]]);export{l as default};
--- assets/index.html-8c70ca62.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,W as t,X as n,Y as _}from"./framework-52f8fb67.js";const c={},o=_("p",null,"一点点",-1),s=[o];function r(a,l){return t(),n("div",null,s)}const i=e(c,[["render",r],["__file","index.html.vue"]]);export{i as default};
