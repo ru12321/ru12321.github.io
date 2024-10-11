1fa71c828da8981a7316c1cdbddde4b2cfc92ca3
ru12321 <48949833+ru12321@users.noreply.github.com>
2024-10-11 07:06:31 +0000

    Deploying to gh-pages from @ ru12321/ru12321.github.io@d333522f4feb1a422a9d5f42d62551752d06eba3 🚀

---
+++ assets/pageview-09a3745b.js
@@ -0,0 +1,1 @@
+import{p as e}from"./app-42cb825c.js";import"./framework-579fb310.js";const a=()=>e({serverURL:"https://comment.goruyi.top"});export{a as updatePageview};
--- assets/pageview-93c8f1e7.js
+++
@@ -1,1 +0,0 @@
-import{p as e}from"./app-14563305.js";import"./framework-579fb310.js";const a=()=>e({serverURL:"https://comment.goruyi.top"});export{a as updatePageview};
